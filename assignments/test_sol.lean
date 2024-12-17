a64c2a355708e9473722bb1a9dbe734ed539738f
aaronletbu <112291697+aaronletbu@users.noreply.github.com>
2024-12-17 10:16:29 -0500

    Add files via upload

---
+++ assignments/test_sol.lean
@@ -0,0 +1,23 @@
+/- Copyright (c) Heather Macbeth, 2023.  All rights reserved. -/
+import Mathlib.Tactic.GCongr
+import Library.Basic
+import AutograderLib
+
+math2001_init
+
+/-! # Homework 0
+
+Don't forget to compare with the text version
+for clearer statements and any special instructions. -/
+
+
+@[autograded 5]
+theorem problem1 {n : ℤ} (hn : n ≥ 5) : n ^ 2 > 2 * n + 11 := by
+  calc
+    n ^ 2 = n * n := by ring
+    _ ≥ 5 * n := by rel [hn]
+    _ = 2 * n + 3 * n := by ring
+    _ ≥ 2 * n + 3 * 5 := by rel [hn]
+    _ = 2 * n + 11 + 4 := by ring
+    _ > 2 * n + 11 := by extra
+  done
